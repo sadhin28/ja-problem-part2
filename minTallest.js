0c0cc9482efea72b5c5f34b3af6e71c0f45e000a
sadhin28 <taosifsadhin113@gmail.com>
2025-04-01 14:07:44 +0600

    update file

--- minTallest.js
+++ minTallest.js
@@ -1,13 +1,13 @@
 const heights = [65,66,68,72,78,60]
-function getMax(numbers){
-    let max =  numbers[0];
+function getMin(numbers){
+    let min =  numbers[0];
     for(let number of numbers){
-        if(number < max){
-            max=number;
+        if(number < min){
+            min=number;
         }
     }
-    return max;
+    return min;
 }
 
-const max = getMax(heights);
-console.log('Max value is : ', max);
+const min = getMin(heights);
+console.log('Min value is : ', min);
